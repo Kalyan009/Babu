50899009f92b8d88670763159108dce6dc326b0b
Kalyan009 <52447265+Kalyan009@users.noreply.github.com>
2019-07-02 15:41:56 +0530

    Update consvow.py

--- consvow.py
+++ consvow.py
@@ -3,6 +3,6 @@ if ch>='a' and ch<='z'
   if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u':
     print("Vowel")
   else:
-    printf("Consonant")
+    print("Consonant")
 else:
   print("Invalid")
